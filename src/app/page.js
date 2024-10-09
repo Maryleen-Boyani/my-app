9bc1b366415b8a102db3b556a99be714b9414dbb
Maryleen Menecha <menechamaryleen@gmail.com>
2024-10-10 02:54:17 +0300

    First commit of the application

--- src/app/page.js
+++ src/app/page.js
@@ -1,95 +1,49 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+import useState from "react";
+import {
+  Box,
+  List,
+  ListItem,
+  Container,
+  Typography,
+  Divider,
+  ListItem,
+  ListItemButton,
+  ListItemText,
+} from "@mui/material";
 
+const navWidhth = 240;
+const navItems = ["Home", "About", "Booking", "Delivery"];
+
+function drawerApp() {
+  const { window } = props;
+  const [isMobile, setIsMobile] = useState(false);
+}
+
+const drawer = (
+  <Box sx={{ bgcolor: "#cdf5f5", height: "100vh", padding: 10 }}>
+    <Typography variant="h5" sx={{ color: "black" }}>
+      FreshThreads
+    </Typography>
+    <Divider />
+    <List>
+      {navItems.map(
+        (
+          item //the arrows are used to write js functions
+        ) => (
+          <ListItem key={item}>
+            <ListItemButton sx={{ textAlign: "center" }}>
+              <ListItemText primary={item} />
+            </ListItemButton>
+          </ListItem>
+        )
+      )}
+    </List>
+  </Box>
+);
+//function that 
+function handleDrawerApp() {
+  setIsMobile((prevState) => !prevState);
+}
 export default function Home() {
-  return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.js</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+  return <Container maxWidth="xl"></Container>;
 }
