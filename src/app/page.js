652efcdd0def046e88a07483e5c092c7c01021ae
Maryleen Menecha <menechamaryleen@gmail.com>
2024-10-14 12:42:07 +0300

    Adjustments to the navbar

--- src/app/page.js
+++ src/app/page.js
@@ -1,4 +1,7 @@
-import useState from "react";
+"use client";
+import { useState } from "react";
+import MenuIcon from "@mui/icons-material/Menu";
+import IconButton from "@mui/material/IconButton";
 import {
   Box,
   List,
@@ -6,44 +9,109 @@ import {
   Container,
   Typography,
   Divider,
-  ListItem,
+  Button,
+  Drawer,
+  CssBaseline,
   ListItemButton,
   ListItemText,
+  AppBar,
+  Toolbar,
 } from "@mui/material";
 
-const navWidhth = 240;
+const drawerWidth = 240;
+//An array of the nav items
 const navItems = ["Home", "About", "Booking", "Delivery"];
 
-function drawerApp() {
+export default function DrawerApp(props) {
   const { window } = props;
+  //setting the current state of the mobile to false
   const [isMobile, setIsMobile] = useState(false);
-}
+  // toggling the mobile drawer open/close state
 
-const drawer = (
-  <Box sx={{ bgcolor: "#cdf5f5", height: "100vh", padding: 10 }}>
-    <Typography variant="h5" sx={{ color: "black" }}>
-      FreshThreads
-    </Typography>
-    <Divider />
-    <List>
-      {navItems.map(
-        (
-          item //the arrows are used to write js functions
-        ) => (
-          <ListItem key={item}>
-            <ListItemButton sx={{ textAlign: "center" }}>
-              <ListItemText primary={item} />
-            </ListItemButton>
-          </ListItem>
-        )
-      )}
-    </List>
-  </Box>
-);
-//function that 
-function handleDrawerApp() {
-  setIsMobile((prevState) => !prevState);
+  const handleDrawerApp = () => {
+    setIsMobile((prevState) => !prevState);
+  };
+
+  //what one sees in the mobile view
+  const drawer = (
+    <Box
+      onClick={handleDrawerApp}
+      sx={{ bgcolor: "#cdf5f5", height: "100vh", padding: 10 }}
+    >
+      <Typography variant="h6" sx={{ my: 2 }}>
+        FreshThreads
+      </Typography>
+
+      <List>
+        {navItems.map(
+          (
+            item //the paranthesis are used to write js functions
+          ) => (
+            <ListItem key={item} disablePadding>
+              <ListItemButton sx={{ textAlign: "center" }}>
+                <ListItemText primary={item} />
+              </ListItemButton>
+            </ListItem>
+          )
+        )}
+      </List>
+    </Box>
+  );
+
+  const container =
+    window !== undefined ? () => window().document.body : undefined;
+  return (
+    //in xl screens
+    <Box sx={{ display: "flex" }}>
+      <CssBaseline />
+      <AppBar component="nav">
+        <Toolbar>
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            edge="start"
+            onClick={handleDrawerApp}
+            sx={{ mr: 2, display: { sm: "none" } }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h5" sx={{ flexGrow: 1 }}>
+            FreshThreads
+          </Typography>
+          <Box
+            sx={{
+              display: { xs: "none", sm: "flex" },
+              justifyContent: "flex-end",
+            }}
+          >
+            {navItems.map((item) => (
+              <Button key={item} sx={{ color: "white" }}>
+                {item}
+              </Button>
+            ))}
+          </Box>
+        </Toolbar>
+      </AppBar>
+
+      <nav>
+        <Drawer
+          container={container}
+          variant="temporary"
+          open={isMobile}
+          onClose={handleDrawerApp}
+          ModalProps={{ keepMounted: true }}
+          sx={{
+            display: { xs: "block", sm: "none" },
+            "& .MuiDrawer-paper": {
+              boxSizing: "border-box",
+              width: drawerWidth,
+            },
+          }}
+        >
+          {drawer}
+        </Drawer>
+      </nav>
+    </Box>
+  );
 }
-export default function Home() {
-  return <Container maxWidth="xl"></Container>;
-}
+//function that is used to reset the state of the isMobile
